413ea2ae7455b3f69e993da577437216ab9594a1
DimiVaksman <vde92@hotmail.com>
2023-02-08 08:40:39 +0100

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const ingredientsEl = document.querySelector('#ingredients');
+console.log(ingredientsEl);
+
+
+ingredients.forEach((ingredientsItem) => {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredientsItem;
+  liEl.classList = 'item';
+  ingredientsEl.append(liEl);
+  
+  console.log(liEl)
+})
+
