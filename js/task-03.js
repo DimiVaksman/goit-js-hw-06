cbe4c9c866dab4ae5f9c84ad10859320a667bb8a
DimiVaksman <vde92@hotmail.com>
2023-02-08 23:55:32 +0100

    task3 up

--- js/task-03.js
+++ js/task-03.js
@@ -15,24 +15,48 @@ const images = [
 
 
 
-
-
 const galleryEl = document.querySelector('.gallery')
 
 
-images.forEach((imagesItem) => {
-  const liEl = document.createElement('li');
-  liEl.classList = 'item';
 
-  const image = document.createElement('img');
 
-  image.src = images.url;
-  image.alt = images.alt;
-  liEl.append(image)
+///////////////////////// Чому коли я додаю черех метод map я отримую src та alt як undefined?
 
-  galleryEl.append(liEl)
+// const getImageEl = images.map((imagesItem) => {
+//   const liEl = document.createElement('li');
+//   liEl.classList = 'item';
+
+//   const image = document.createElement('img');
+
+//   image.setAttribute('id', 'image');
+//   image.src = images.url; 
+//   // Чомусь не працює
+//   image.alt = images.alt; 
+//   // Чомусь не працює
+
+//   liEl.append(image)
+
+//   galleryEl.append(liEl)
+//   console.log(liEl)
   
-});
+// });
 
 
-console.log(galleryEl)
+
+
+galleryEl.insertAdjacentHTML('afterbegin' , `
+<li class="liadd"><img class="imgadd" src="https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="White and Black Long Fur Cat"></li>
+<li class="liadd"><img class="imgadd" src="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Orange and White Koi Fish Near Yellow Koi Fish"></li>
+<li class="liadd"><img class="imgadd" src="https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Group of Horses Running"></li>`  )
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/task-04.js
@@ -0,0 +1,34 @@
+
+// let counterValue = document.querySelector('#value')
+
+// const counterValue = [0];
+
+const getDecrement = counterValue.map(value => value +=1 );
+console.log(getDecrement)
+
+
+
+
+
+const divEl = document.querySelector('#counter')
+console.log(divEl)
+
+const btnDecrementEl = divEl.firstElementChild
+
+// btnDecrementEl.addEventListener('click' , (event) => {
+//     counterValue.map(value => value +=1)
+// })
+
+
+btnDecrementEl.addEventListener('click' , () => {
+;
+})
+
+
+    let counterValue = 0;
+    return function() {
+      return counterValue+=1;
+    }
+
+
+
