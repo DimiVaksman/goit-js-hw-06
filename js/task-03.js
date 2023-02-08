2b4b5c4afca057f2e86b5cb2e1d49e32b9524bd6
DimiVaksman <vde92@hotmail.com>
2023-02-09 00:28:56 +0100

    task3

--- js/task-03.js
+++ js/task-03.js
@@ -16,11 +16,18 @@ const images = [
 
 
 const galleryEl = document.querySelector('.gallery')
+const liEl = document.createElement('li');
+liEl.classList = 'item';
+
+const image = document.createElement('img');
+
+image.setAttribute('id', 'image');
+image.src = images.url; 
+image.alt = images.alt; 
 
 
 
-
-///////////////////////// Чому коли я додаю черех метод map я отримую src та alt як undefined?
+///////////////////////// Чому коли я додаю через метод map я отримую src та alt як undefined?
 
 // const getImageEl = images.map((imagesItem) => {
 //   const liEl = document.createElement('li');
@@ -49,6 +56,17 @@ galleryEl.insertAdjacentHTML('afterbegin' , `
 <li class="liadd"><img class="imgadd" src="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Orange and White Koi Fish Near Yellow Koi Fish"></li>
 <li class="liadd"><img class="imgadd" src="https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Group of Horses Running"></li>`  )
 
+
+
+
+
+
+
+
+// galleryEl.insertAdjacentHTML('afterbegin' , `${images.reduce(
+//   (acum, imagesEl) => (acum += `<li>${imagesEl}</li>`),
+//   ''
+// )}`  )
 
 
 
