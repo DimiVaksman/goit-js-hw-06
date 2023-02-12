eba70345553f0b5c4357b6fe63f01a41af022473
DimiVaksman <vde92@hotmail.com>
2023-02-12 20:28:54 +0100

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
+
+
+const btnChangeColor = document.querySelector('.change-color');
+console.log(btnChangeColor)
+
+const body = document.querySelector('body')
+console.log(body)
+
+
+btnChangeColor.addEventListener('click' , (event) => {
+
+
+
+body.style.backgroundColor = getRandomHexColor()
+
+console.log('test change color');
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
